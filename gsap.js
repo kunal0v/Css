08427118681372db51a9a2db304b5149aa6e13d5
kunal0v <bhandarikunal26@gmail.com>
2024-09-03 18:16:39 +0530

    gsap

---
+++ gsap.js
@@ -0,0 +1,24 @@
+gsap.to(".parent",{
+    x:800,
+    duration:1,
+    delay:1,
+    rotate:360,
+    scale:0.1,
+    backgroundColor:"green",
+})
+gsap.to(".parent1",{
+    x:800,
+    duration:1,
+    delay:2,
+    rotate:360,
+    scale:0.1,
+    backgroundColor:"green",
+})
+gsap.to(".parent2",{
+    x:800,
+    duration:1,
+    delay:3,
+    rotate:360,
+    scale:0.1,
+    backgroundColor:"green",
+})
